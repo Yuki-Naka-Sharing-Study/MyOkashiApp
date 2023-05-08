94227fba79953672fcfc80d7b6d2d095253c8b9a
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-05-08 20:03:58 +0900

    Initial Commit

---
+++ MyOkashiApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyOkashiApp
+//
+//  Created by 仲優樹 on 2023/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashiApp/MyOkashiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiAppApp.swift
+//  MyOkashiApp
+//
+//  Created by 仲優樹 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
